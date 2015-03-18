bd3f134047ca557899fe490d57acd8fde6502f92
Matthew Smith <mtscout6@gmail.com>
2015-03-18 09:21:27 -0600

    Add intial script for loader

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(source) {
+  var extension = path.extname(this.resourcePath);
+  var basename = path.basename(this.resourcePath, extension);
+  var dirname = path.dirname(this.resourcePath);
+
+  var clientPath = path.join(dirname, basename + '.client' + extension);
+
+  if (fs.existsSync(clientPath)) {
+    var clientSource = fs.readFileSync(clientPath, 'utf-8');
+    return clientSource;
+  }
+
+  return source;
+};
